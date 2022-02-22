1fcb5db59a795e565ffc752344087522944ddbb7
Deep Patel <deep.patel@solutionanalysts.in>
2022-02-22 22:54:25 +0530

    Deleted addItems and that code is added into script

--- addItems.js
+++
@@ -1,6 +0,0 @@
-function openPopup() {
-  document.getElementsByClassName("popup")[0].classList.add("active");
-}
-function closePopup() {
-  document.getElementsByClassName("popup")[0].classList.remove("active");
-}
