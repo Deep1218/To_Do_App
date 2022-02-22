676711663fb9c3b995cabb23b745efa8f431472a
Deep Patel <deep.patel@solutionanalysts.in>
2022-02-22 14:48:47 +0530

    add items Html and Css completed

---
+++ addItems.js
@@ -0,0 +1,6 @@
+function openPopup() {
+  document.getElementsByClassName("popup")[0].classList.add("active");
+}
+function closePopup() {
+  document.getElementsByClassName("popup")[0].classList.remove("active");
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("add-btn").addEventListener("click", openPopup);
+
+document.getElementById("close-btn").addEventListener("click", closePopup);
